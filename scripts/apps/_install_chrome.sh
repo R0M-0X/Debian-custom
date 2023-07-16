8b8542923a436f142a09c5853b7929a5ceb5b1f0
Romain BERBILLE <romain@rom.ovh>
2023-07-16 10:34:53 +0200

    add scripts

---
+++ scripts/apps/_install_chrome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Source -> https://www.geekersdigest.com/how-to-install-google-chrome-from-apt-repository/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/chrome.list
+sudo apt-get update
+sudo apt-get install google-chrome-stable -yy
---
+++ scripts/post_install_perso.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt update -yy
+apt install net-tools apache2 mariadb-server php curl git sudo htop -yy
+apt install nmap -yy
+apt install neofetch conky -yy
+
+
+#apt install -y ftp open-vm-* xfce4 tilix python3-pip python3 gedit dbus-x11 unzip
+#apt install -y papirus-icon-theme python3-venv gedit proxychains4 freerdp2-x11
+#apt install -y lightdm-settings lightdm slick-greeter xfce4-goodies libpcap-dev wget
+#apt install -y libssl-dev tcpdump firefox-esr unzip sudo smbclient imagemagick ghostscript gnome-themes-extra gnome-themes-extra-data
+#apt install -y tshark simplescreenrecorder
