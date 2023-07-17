81c1e25ca4f7ccffb0d8511f4764230d5dc9bd31
root <root@ROM-LAPTOP-ASUS>
2023-07-17 16:14:34 +0200

    17072023-1614

--- scripts/apps/_install_chrome.sh
+++ scripts/apps/_install_chrome.sh
@@ -2,5 +2,7 @@
 # Source -> https://www.geekersdigest.com/how-to-install-google-chrome-from-apt-repository/
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/chrome.list
-sudo apt-get update
-sudo apt-get install google-chrome-stable -yy
+apt update -yy
+apt install google-chrome-stable -yy
+rm /etc/apt/sources.list.d/chrome.list
+apt update -yy
--- scripts/post_install_perso.sh
+++ scripts/post_install_perso.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
+####### Dont run as root, run the script with sudo
 apt update -yy
 apt install net-tools apache2 mariadb-server php curl git sudo htop -yy
 apt install nmap -yy
 apt install neofetch conky -yy
+apt install ftp open-vm-* unzip sudo smbclient -yy
+apt install fasttrack-archive-keyring -yy
+apt install onedrive
+# onedrive --synchronize (attention lance la première synchro)
 
+##### Install Wine
+dpkg --add-architecture i386
+mkdir -pm755 /etc/apt/keyrings
+wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+apt update -yy
+apt install --install-recommends winehq-stable -yy
 
-#apt install -y ftp open-vm-* xfce4 tilix python3-pip python3 gedit dbus-x11 unzip
-#apt install -y papirus-icon-theme python3-venv gedit proxychains4 freerdp2-x11
-#apt install -y lightdm-settings lightdm slick-greeter xfce4-goodies libpcap-dev wget
-#apt install -y libssl-dev tcpdump firefox-esr unzip sudo smbclient imagemagick ghostscript gnome-themes-extra gnome-themes-extra-data
-#apt install -y tshark simplescreenrecorder
+apt install virt-manager virt-viewer virt-v2v virt-p2v -yy
+apt install cockpit cockpit-machines -yy
+systemctl enable cockpit
+systemctl start cockpit
+# localhost http://localhost:9090
+
+sudo usermod -aG sudo $USER
+
+#### Bashrc à faire
+# PXE server & DHCP no start auto
+
+# Apache
+rm -r /var/www/html
+touch /var/www/index.html
