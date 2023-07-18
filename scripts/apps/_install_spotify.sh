01c9e14ffe07bb8e2f96b731e84a93065cdfe264
Romain BERBILLE <romain@rom.ovh>
2023-07-18 13:57:50 +0200

    180723-1357

--- scripts/apps/_install_spotify.sh
+++ scripts/apps/_install_spotify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # https://www.spotify.com/us/download/linux/
+# -------------------------------------------------------------------------------------------------------------------------------------------
 curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client -yy
+# Si la première connexion ne fonctionne pas, il faut vérifier le navigarteur par defaut (ne fonctionne pas avec Konqueror)
--- scripts/post_install_perso.sh
+++ scripts/post_install_perso.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+# Faire un depôt pour une gestion des paquets plus simple
+
+#Check release -> $(lsb_release -cs)
+
 ####### Dont run as root, run the script with sudo
 apt update -yy
-apt install net-tools apache2 mariadb-server php curl git sudo htop -yy
-apt install nmap -yy
+# Basic
+apt install net-tools curl git sudo htop ftp open-vm-* unzip sudo smbclient -yy
+# Web
+apt install apache2 mariadb-server php -yy
+# Custom
 apt install neofetch conky -yy
-apt install ftp open-vm-* unzip sudo smbclient -yy
-apt install fasttrack-archive-keyring -yy
-apt install onedrive
+# Redteam tools
+apt install nmap -yy
+# Multimedia
+apt install vlc -yy
+# Other
+apt install onedrive obsidian -yy
+
+
 # onedrive --synchronize (attention lance la première synchro)
 
 ##### Install Wine
@@ -17,7 +29,6 @@ wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists
 apt update -yy
 apt install --install-recommends winehq-stable -yy
 
-apt install virt-manager virt-viewer virt-v2v virt-p2v -yy
 apt install cockpit cockpit-machines -yy
 systemctl enable cockpit
 systemctl start cockpit
@@ -25,9 +36,40 @@ systemctl start cockpit
 
 sudo usermod -aG sudo $USER
 
-#### Bashrc à faire
 # PXE server & DHCP no start auto
 
 # Apache
 rm -r /var/www/html
 touch /var/www/index.html
+
+# Ulauncher
+sudo apt update && sudo apt install -y gnupg
+gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
+gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] \
+          http://ppa.launchpad.net/agornostal/ulauncher/ubuntu jammy main" \
+          | sudo tee /etc/apt/sources.list.d/ulauncher-jammy.list
+sudo apt update && sudo apt install ulauncher
+
+### Sec tools
+
+python3 -m pip install pipx ## for pipx apps
+python3 -m pip install poetry ## For poetry-enabled apps
+pipx ensurepath ## Fixes pipx $PATH
+pipx install mitm6 ## MITM6 is great
+
+## Build out the Red Team directory for all self-managed tools:
+mkdir -p /redteam/windows-domains
+mkdir -p /redteam/wifi
+mkdir -p /redteam/osint
+mkdir -p /redteam/web
+mkdir -p /redteam/exploit
+mkdir -p /redteam/passwords
+mkdir -p /redteam/wordlists
+mkdir -p /redteam/printers
+
+## Build out Pentest area:
+mkdir -p /root/Pentest/web
+mkdir -p /root/Pentest/scans
+mkdir -p /root/Pentest/files
+mkdir -p /root/Pentest/users
